d3fe57008167bc630bcef35d53c10f8333bb7438
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-03-30 12:47:06 +0300

    Finish task-01

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const listCategoriesRef = document.querySelector('#categories');
+
+console.log(`В списке ${listCategoriesRef.children.length} категории.`);
+
+const listItemsRef = document.querySelectorAll('.item');
+
+const getInfoRef = list => {
+  return list.forEach(item => {
+    console.log(`Категория: ${item.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${item.querySelectorAll('li').length}`);
+  });
+};
+
+getInfoRef(listItemsRef);

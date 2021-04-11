2ad90e5fff899c3c5ec164fdb9752428a150bcfa
Rozumiak Vladyslav <lunatiq86@gmail.com>
2021-04-11 11:37:43 +0300

    Finished task-05

--- js/task-05.js
+++ js/task-05.js
@@ -7,4 +7,7 @@ refs.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
   refs.nameOutput.textContent = event.currentTarget.value;
+  if (refs.nameOutput.textContent.length === 0) {
+    refs.nameOutput.textContent = 'незнакомец';
+  }
 }
